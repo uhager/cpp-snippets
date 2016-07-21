bc251e41adc6ee5c865c979b7252c63515c6f833
uhager <uhager42@gmail.com>
2016-07-20 20:51:19 -0400

    Using vectors instead of arrays. Using doubles instead of int. Finally seems to work. Brute force method really is quite slow.

--- multiply_array.cc
+++ multiply_array.cc
@@ -8,14 +8,15 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 #include <cstdlib>
+#include <chrono>
 
-
-void brute_force(int* arr, int size, int* result)
+void brute_force(const std::vector<double>& arr, double* result)
 {
-  for (int i = 0; i < size; ++i ){
+  for (uint32_t i = 0; i < arr.size(); ++i ){
     result[i] = 1;
-    for (int j = 0; j < size; ++j ){
+    for (uint32_t j = 0; j < arr.size(); ++j ){
       if (i == j) continue;
       result[i] *= arr[j];
     }
@@ -23,80 +24,88 @@ void brute_force(int* arr, int size, int* result)
 }
 
 
-void using_division(int* arr, int size, int* result)
+void using_division(const std::vector<double>& arr, double* result)
 {
-  int total = 1;
-  for (int i = 0; i < size; ++i ){
-    total *= arr[i];
+  double total = 1;
+  for (uint32_t i = 0; i < arr.size(); ++i ){
+    total *= arr.at(i);
   }
-  for (int i = 0; i < size; ++i ){
-    result[i] = total / arr[i];
+  for (uint32_t i = 0; i < arr.size(); ++i ){
+    result[i] = total / arr.at(i);
   }
 }
 
 
-void forwards_backwards(int* arr, int size, int* result)
+void forwards_backwards(const std::vector<double>& arr, double* result)
 {
-  int left_to_right[size];
-  int right_to_left[size];
+  uint32_t size = arr.size();
+  double left_to_right[size];
+  double right_to_left[size];
   left_to_right[0] = 1;
   right_to_left[size-1] = 1;
-  for (int i = 1 ; i<size ; ++i) {
+  for (uint32_t i = 1 ; i<size ; ++i) {
     left_to_right[i] = left_to_right[i-1] * arr[i-1];
+    right_to_left[size-1-i] = right_to_left[size-i] * arr[size-i];
   }
-  for (int i = size-2 ; i >= 0 ; --i) {
-    right_to_left[i] = right_to_left[i+1] * arr[i+1];
-  }
-
-  for (int i = 0 ; i<size ; ++i) {
+  for (uint32_t i = 0 ; i < size ; ++i) {
     result[i] = left_to_right[i] * right_to_left[i];
   }
 }
 
 
-void calc_and_print(void (*fptr)(int*, int, int*), int* arr, int size, const std::string& title = "Result")
+void calc_and_print(void (*fptr)(const std::vector<double>&, double*), const std::vector<double>& arr, const std::string& title = "Result")
 {
-  int result[size];
+  double result[arr.size()];
   std::cout << title << ": ";
-  fptr(arr, size, result);
-  for (int i = 0; i<size; ++i) {
+
+  auto start_time = std::chrono::high_resolution_clock::now();
+  fptr(arr, result);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  double time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
+
+  for (uint32_t i = 0; i<arr.size(); ++i) {
     std::cout << result[i] << ", " ;
   }
-  std::cout << "\n";
+  std::cout << "\ntime: " << std::to_string(time) << "\n";
 }
 
 
-void get_input_array(int* arr, int& size)
+void get_input_array(std::vector<double>& arr)
 {
   std::string arr_string;
 
   std::cout << "Enter comma-separated array: ";
   std::getline( std::cin, arr_string );
 
-  int* curr = arr;
   while (arr_string.size()) {
-    std::string::size_type comma = arr_string.find_first_of(",");
+    std::string::size_type comma = arr_string.find_first_of(',');
     if ( comma > 0 ) {
-      *curr++ = std::stoi(arr_string.substr(0,comma));
-      ++size;
+      arr.push_back( std::stod(arr_string.substr(0,comma)) );
     }
     arr_string.erase(0,comma+1);
     if (comma == std::string::npos) break;
   }
+
+  /*
+  std::cout << "read array: " ;
+  for (uint32_t i = 0; i < arr.size(); ++i) {
+    std::cout << arr[i] << ", " ;
+  }
+  */
+  std::cout << "\n" ;  
 }
 
 
 int main()
 {
-  int size = 0;
-  int* arr = new int();
+  std::vector<double> arr ;
 
-  get_input_array(arr, size);
+  get_input_array(arr);
 
-  void (*methods[])(int*, int, int*) = { brute_force, using_division, forwards_backwards};
+  void (*methods[])(const std::vector<double>&, double*) = { brute_force, using_division, forwards_backwards};
   std::string outputs[] = {"Brute force", "With division: ", "Forwards-backwards partial products: "};
 
-  for ( int i = 0 ; i<3; ++i ) {
-    calc_and_print(*methods[i], arr, size,outputs[i] );
+  for ( uint32_t i = 0 ; i<3; ++i ) {
+    calc_and_print(*methods[i], arr, outputs[i] );
   }
 }
